de9b62a90c9c93d3e8be398f4842f8eac7a9b0fc
Александра Борисевич <df_67@tut.by>
2020-10-02 12:54:22 +0300

    feat: fulfil task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(ar) {
+    if (arguments.length == 0 || ar.length == 0) {
+    return "0";}
+return Math.min.apply(Math, ar);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(ar) {
+    if (arguments.length == 0 || ar.length == 0) {
+    return "0";}
+return Math.max.apply(Math, ar);
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
+exports.avg = function avr(ar) {
+
+    
+        if (arguments.length == 0 || ar.length == 0) {
+        return "0";}
+        
+        var result = ar.reduce((a, b) => a + b, 0);
+        var total = result / ar.length;
+    
+    return total;
+    }
+    
